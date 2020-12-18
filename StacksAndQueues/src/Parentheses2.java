f9ac14e0286e5b8b19f2fde0c0b148a70efff3cf
Polina Pavlova <poli.paskaleva@gmail.com>
2020-12-18 11:26:22 +0200

    wrong output

---
+++ StacksAndQueues/src/Parentheses2.java
@@ -0,0 +1,38 @@
+import java.util.ArrayDeque;
+import java.util.Scanner;
+
+public
+class Parentheses2 {
+    public static
+    void main (String[] args) {
+        Scanner               scanner    = new Scanner (System.in);
+        String                line       = scanner.nextLine ();
+        boolean               isBalanced = true;
+        ArrayDeque<Character> stack      = new ArrayDeque<> ();
+
+
+        for (int i = 0; i < line.length (); i++) {
+            if (line.charAt (i) == '[' || line.charAt (i) == '{' || line.charAt (i) == '(') {
+                stack.push (line.charAt (i));
+            } else {
+                if (stack.isEmpty ()) {
+                    isBalanced = false;
+                    break;
+                }
+                char currant = line.charAt (i);
+                if (currant=='}'&&stack.pop ()!='{'){
+                    isBalanced = false;break;
+                }else if (currant==']'&&stack.pop ()!='['){
+                    isBalanced = false;break;
+                }else if (currant==')'&&stack.pop ()!='('){
+                    isBalanced = false;break;
+                }
+            }
+        }
+        if (!isBalanced) {
+            System.out.println ("NO");
+        } else {
+            System.out.println ("YES");
+        }
+    }
+}
