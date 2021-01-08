a85d7b3650b18cb4b77e8d6d5a0c6ca1b22f9c33
Polina Pavlova <poli.paskaleva@gmail.com>
2021-01-08 12:45:15 +0200

    homework problems

---
+++ Generics/src/GenericBox/Box.java
@@ -0,0 +1,26 @@
+package GenericBox;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public
+class Box<T>{
+    private List<T> values;
+
+    public Box(){
+        values = new ArrayList<> ();
+    }
+    public void addElement (T element){
+        this.values.add (element);
+    }
+
+    @Override
+    public
+    String toString () {
+        StringBuilder sb = new StringBuilder ();
+        for (T value : values) {
+           sb.append (String.format ("%s: %s%n", value.getClass ().getName (),value));
+        }
+        return sb.toString ();
+    }
+}
