8d9808c7a6259d05b0f409cd03f8a6deb347c941
Polina Pavlova <poli.paskaleva@gmail.com>
2021-01-08 13:01:00 +0200

    homework problems

---
+++ Generics/src/GenericBoxОfInteger/Main.java
@@ -0,0 +1,24 @@
+package GenericBoxОfInteger;
+
+import GenericBox.Box;
+
+import java.util.Scanner;
+
+public
+class Main {
+    public static
+    void main (String[] args) {
+
+        Scanner scanner = new Scanner (System.in);
+
+        int n = Integer.parseInt (scanner.nextLine ());
+
+        Box<Integer> box = new Box<> ();
+
+        for (int i = 0; i < n; i++) {
+            int integer = Integer.parseInt (scanner.nextLine ());
+            box.addElement (integer);
+        }
+        System.out.println (box);
+    }
+}
