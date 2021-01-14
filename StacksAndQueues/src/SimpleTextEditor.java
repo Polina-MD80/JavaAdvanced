34c27f5ac55980a4cee2c41221c0fc7e38c5b178
Polina Pavlova <poli.paskaleva@gmail.com>
2021-01-14 21:32:01 +0200

    homework problems

--- StacksAndQueues/src/SimpleTextEditor.java
+++ StacksAndQueues/src/SimpleTextEditor.java
@@ -6,7 +6,7 @@ class SimpleTextEditor {
     public static
     void main (String[] args) {
         Scanner              scanner = new Scanner (System.in);
-        String text = "";
+        String               text    = "";
         ArrayDeque<String[]> stack   = new ArrayDeque<> ();
 
         int n = Integer.parseInt (scanner.nextLine ());
@@ -16,31 +16,31 @@ class SimpleTextEditor {
 
             switch (command[0]) {
                 case "1":
-                  text =  text.concat (command[1]);
+                    text = text.concat (command[1]);
                     stack.push (command);
                     break;
                 case "2":
                     int count = Integer.parseInt (command[1]);
                     command[1] = text.substring (text.length () - count);
-                    text = text.substring (0,text.length ()-count);
+                    text = text.substring (0, text.length () - count);
                     stack.push (command);
                     break;
                 case "3":
                     int index = Integer.parseInt (command[1]);
-                    System.out.println (text.charAt (index-1));
+                    System.out.println (text.charAt (index - 1));
                     break;
                 case "4":
                     if (!stack.isEmpty ()) {
                         String[] undoCommand = stack.pop ();
-                        switch (undoCommand[0]){
-                            case"1":
-                                text = text.substring (0,text.length ()-undoCommand[1].length ());
+                        switch (undoCommand[0]) {
+                            case "1":
+                                text = text.substring (0, text.length () - undoCommand[1].length ());
                                 break;
-                            case"2":
-                                text = text .concat ( undoCommand[1]);
+                            case "2":
+                                text = text.concat (undoCommand[1]);
                                 break;
                         }
-                            break;
+                        break;
                     }
             }
         }
