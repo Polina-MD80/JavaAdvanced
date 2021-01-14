1ab05172aa0295a5d9d0f78ecdcd8b7f3b10ae19
Polina Pavlova <poli.paskaleva@gmail.com>
2021-01-14 19:25:02 +0200

    homework problems

--- StacksAndQueues/src/ReverseNumbersWithStack.java
+++ StacksAndQueues/src/ReverseNumbersWithStack.java
@@ -2,20 +2,19 @@ import java.util.ArrayDeque;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public
 class ReverseNumbersWithStack {
     public static
     void main (String[] args) {
         Scanner scanner = new Scanner (System.in);
-        String[] input = scanner.nextLine ().split ("\\s+");
 
-        ArrayDeque<String> stack = new ArrayDeque<>();
+        ArrayDeque<Integer> stack = new ArrayDeque<>();
 
+        Arrays.stream (scanner.nextLine ().split ("\\s+")).mapToInt (Integer::parseInt)
+                .forEach (stack::push);
 
-        for (String s : input) {
-            stack.push (s);
-        }
         while (!stack.isEmpty ()){
             System.out.print (stack.pop ()+ " ");
         }
