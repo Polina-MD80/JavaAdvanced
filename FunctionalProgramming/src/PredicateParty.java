5774fbf7e7910173aed020aa32e1c49f5b64d69a
Polina Pavlova <poli.paskaleva@gmail.com>
2021-01-25 12:50:31 +0200

    homework problems

--- FunctionalProgramming/src/PredicateParty.java
+++ FunctionalProgramming/src/PredicateParty.java
@@ -1,6 +1,7 @@
 import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
+import java.util.function.BiPredicate;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
@@ -12,30 +13,43 @@ class PredicateParty {
 
         List<String> names = Arrays.stream (scanner.nextLine ().split ("\\s+")).collect (Collectors.toList ());
         String       command;
+
+
         while (!"Party!".equals (command = scanner.nextLine ())) {
             String[] tokens = command.split ("\\s+");
-            Predicate<String> check = name -> {
-                switch (tokens[0]){
-                    case "Remove":
-                        if (tokens[1].equals ("StartsWith")){
-                            if (name.startsWith (tokens[2])){
-                                return false;
-                            }
-                        }else {
-                            if (name.endsWith (tokens[2])){
-                                return false;
-                            }
-                        }
-                        break;
-                    case "Double":
+            BiPredicate<String, String> filter = checkForPredicate (tokens[1]);
+            switch (tokens[0]) {
+                case "Remove":
+                     names = names.stream ().filter (n -> !filter.test (n, tokens[2])).collect (Collectors.toList ());
+                     break;
+                case "Double":
+                    List<String> namesToAdd = names.stream ().filter ((n -> filter.test (n, tokens[2]))).collect (Collectors.toList ());
+                    names.addAll (namesToAdd);
+                    break;
+            }
 
-                        break;
-                }
-                return true;
-            };
-
-            List<String> collect = names.stream ().filter (check).collect (Collectors.toList ());
-            System.out.println ();
         }
+        if (names.isEmpty ()){
+            System.out.println ("Nobody is going to the party!");
+        }else {
+            System.out.print (names.stream ().sorted (String::compareTo).collect (Collectors.joining (", ")));
+            System.out.println (" are going to the party!");
+        }
+    }
+
+    private static
+    BiPredicate<String, String> checkForPredicate (String act) {
+        switch (act) {
+            case "StartsWith":
+                return String::startsWith;
+
+            case "EndsWith":
+                return String::endsWith;
+
+            case "Length":
+                return (string, n) -> string.length () == Integer.parseInt (n);
+
+        }
+        return null;
     }
 }
