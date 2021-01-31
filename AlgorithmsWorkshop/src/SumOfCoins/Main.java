7a811892620391376aefa515a1ceb8561b65e842
Polina Pavlova <poli.paskaleva@gmail.com>
2021-01-31 10:48:56 +0200

    homework problems

---
+++ AlgorithmsWorkshop/src/SumOfCoins/Main.java
@@ -0,0 +1,43 @@
+package SumOfCoins;
+import java.util.*;
+import java.util.stream.Collectors;
+
+public
+class Main {
+    public static
+    void main (String[] args) {
+        Scanner in = new Scanner (System.in);
+
+        String[] elements = in.nextLine ().substring (7).split (", ");
+        int[]    coins    = new int[elements.length];
+        for (int i = 0; i < coins.length; i++) {
+            coins[i] = Integer.parseInt (elements[i]);
+        }
+
+        int targetSum = Integer.parseInt (in.nextLine ().substring (5));
+
+
+        Map<Integer, Integer> usedCoins = chooseCoins (coins, targetSum);
+
+        for (Map.Entry<Integer, Integer> usedCoin : usedCoins.entrySet ()) {
+            System.out.println (usedCoin.getKey () + " -> " + usedCoin.getValue ());
+        }
+    }
+
+    public static
+    Map<Integer, Integer> chooseCoins (int[] coins, int targetSum) {
+        Map<Integer, Integer> result = new LinkedHashMap<> ();
+
+        for (int i = coins.length - 1; i >= 0; i--) {
+            int currantCoin = coins[i];
+            if (targetSum / currantCoin != 0) {
+                result.put (currantCoin,targetSum/currantCoin);
+            }
+            targetSum = targetSum%currantCoin;
+            if (targetSum==0){
+                break;
+            }
+        }
+        return result;
+    }
+}
