b8b21935a81cf7522216f968402748d31e7c201e
Polina Pavlova <poli.paskaleva@gmail.com>
2021-02-01 09:14:14 +0200

    homework problems

---
+++ AlgorithmsWorkshop/src/BinarySearch.java
@@ -0,0 +1,33 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public
+class BinarySearch {
+    public static
+    void main (String[] args) {
+        Scanner scanner = new Scanner (System.in);
+        int[] array = Arrays.stream (scanner.nextLine ().split ("\\s+")).mapToInt (Integer::parseInt).toArray ();
+        int key = Integer.parseInt (scanner.nextLine ());
+
+        System.out.println (getIndex (array, key));
+    }
+
+    private static
+    int getIndex (int[] array, int key) {
+        int l = 0;
+        int r = array.length - 1;
+
+        while (l<=r){
+            int mid = l + (r-l)/2;
+            if (array[mid]>key){
+                r = mid -1;
+            }else if (array[mid]<key){
+                r = mid + 1;
+            }else return mid;
+
+
+        }
+        return -1;
+
+    }
+}
---
+++ AlgorithmsWorkshop/src/Quicksort.java
@@ -0,0 +1,55 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public
+class Quicksort {
+    public static
+    void main (String[] args) {
+        Scanner scanner = new Scanner (System.in);
+
+        int[] array = Arrays.stream (scanner.nextLine ().split ("\\s+")).mapToInt (Integer::parseInt).toArray ();
+        int l = 0;
+        int r = array.length -1;
+        array = quickSort (array,l,r);
+        for (int j : array) {
+            System.out.print (j + " ");
+        }
+    }
+
+    private static
+    int[] quickSort (int[] array, int l, int r) {
+        if (l>=r){
+            return array;
+        }
+       int pi = partition(array,l,r);
+        quickSort (array,l,pi-1);
+        quickSort (array,pi+1,r);
+
+        return array;
+
+
+    }
+
+    private static
+    int partition (int[] array, int l, int r) {
+
+        int pivot = array[r];
+
+        int i = l-1;
+        for (int j = l; j < r ; j++) {
+            if (array[j] < pivot){
+                i++;
+                int temp = array[i];
+                array[i] = array[j];
+                array[j]= temp;
+            }
+
+        }
+        int temp = array[i+1];
+        array[i+1] = array[r];
+        array[r]=temp;
+        return i+1;
+
+    }
+
+}
